ca7dbc9ec3ed33e5cdd774f7ef8e79848c92fa63
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-07-18 02:45:49 -0300

    incrementação da logica de entrada e saidas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Form from "./components/Form";
 import Header from "./components/Header";
 import Summary from "./components/Summary";
 import GlobalStyle from "./styles/global";
 
 function App() {
+  const data = localStorage.getItem("transactions");
+  const [transactionsList, setTransactionsList] = useState(
+    data ? JSON.parse(data) : []
+  );
+
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transaction) => Number(transaction.amount));
+
+    const amountIncome = transactionsList
+      .filter((item) => !item.expense)
+      .map((transaction) => Number(transaction.amount));
+
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+    const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+    const total = Math.abs(income - expense).toFixed(2);
+
+    setExpense(`R$ ${expense}`);
+    setIncome(`R$ ${income}`);
+    setTotal(`${Number(income) < Number(expense) ? "-" : ""} R$ ${total}`);
+  }, [transactionsList]);
+
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Summary />
+      <Summary income={income} expense={expense} total={total} />
+      <Form />
     </>
   );
 }
